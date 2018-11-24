49cb2090fe1657fa5c9284f0e8fdae6e0598e4df
Toan <toan@tabvn.com>
2018-11-24 22:07:44 +0700

    Update

--- server/git.sh
+++ server/git.sh
@@ -1,5 +1,5 @@
 rm schema.sql
-mysqldump -u --no-data root ued > schema.sql
+mysqldump -u root ued > schema.sql
 cp -r /Users/toan/go/src/ued/ /Users/toan/Projects/judge/server
 cp -r /Users/toan/Projects/ued_web/src /Users/toan/Projects/judge/web/src
 cp -r /Users/toan/Projects/ued_web/public /Users/toan/Projects/judge/web/public
