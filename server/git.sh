1bc626bd081ed89b714a57706cf6fa6d3c003fc2
Toan <toan@tabvn.com>
2018-12-07 16:37:19 +0700

    Update

--- server/git.sh
+++ server/git.sh
@@ -5,6 +5,7 @@ cp -r /Users/toan/Projects/ued_web/src/ /Users/toan/Projects/judge/web/src/
 cp -r /Users/toan/Projects/ued_web/public /Users/toan/Projects/judge/web/public
 cp -r /Users/toan/Projects/ued_web/package.json /Users/toan/Projects/judge/web/
 cd /Users/toan/Projects/judge/
+git pull
 git add -A
 git commit -m "Update"
 git push
